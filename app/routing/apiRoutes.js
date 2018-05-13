2b753c9d523367a4ce8e3257a77d339e154d210e
Joseph Holder <josephholder@Josephs-MacBook-Pro.local>
2018-05-13 19:30:49 -0400

    Routing fixes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,13 @@
-var friends = require("../data/friends.js");
+var friends = require("../data/friends");
 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
     app.post("/api/friends", function (req, res) {
+        // code to match friends
         friends.push(req.body);
+
+        //math logic for the best match of a friend
     });
 };
